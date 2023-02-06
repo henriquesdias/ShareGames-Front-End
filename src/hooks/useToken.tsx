2e4d3b2a4cd88e7900d22838948223fe282a3b84
henriquesdias <henrique.dias1915@gmail.com>
2023-02-06 17:39:23 -0300

    fix: removing bug with token, when it's do not exists

--- src/hooks/useToken.tsx
+++ src/hooks/useToken.tsx
@@ -4,7 +4,5 @@ import { UserContext } from "../context/User-Context";
 
 export default function useToken() {
   const { token, setToken } = useContext(UserContext);
-  const base64Url = token.split(".")[1];
-  const info = JSON.parse(atob(base64Url));
   return { token, setToken };
 }
